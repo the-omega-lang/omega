966e542865c583ee41f63a76fbf618f3d46ad41f
Rdbo <rdbodev@gmail.com>
2026-04-11 02:15:45 +0000

    simple string parser to avoid issues

--- omega-parser/src/syntax/expression/string.rs
+++ omega-parser/src/syntax/expression/string.rs
@@ -6,10 +6,10 @@ pub struct StringExpr(pub String);
 
 impl StringExpr {
     parser!(() => Self {
-        // just('"')
-        //     .ignore_then(none_of('"').repeated().to_slice().map(ToString::to_string))
-        //     .then_ignore(just('"'))
-        //     .map(|s| Self(s))
+        just('"')
+            .ignore_then(none_of('"').repeated().to_slice().map(ToString::to_string))
+            .then_ignore(just('"'))
+            .map(|s| Self(s))
 
         // TODO: Implement odd-lengthed delimiter strings, similar to as follows
         // let quote_start_parser = just('"').repeated().at_least(1).count();
@@ -35,18 +35,19 @@ impl StringExpr {
         //     .map(|s| StringExpr(s))
 
 
-        let empty_string = just('"').repeated().exactly(2).padded().map(|_| StringExpr("".to_owned()));
+        // NOTE: The below implementation somewhat worked, but now sometimes it matches things that dont start with "
+        // let empty_string = just('"').repeated().exactly(2).padded().map(|_| StringExpr("".to_owned()));
 
-        let delim_start = just('"').repeated().exactly(1).ignore_then(just("\"\"").repeated().count().map(|count| count * 2 + 1)).or(just('"').not().map(|_| 0));
-        let delim_end = just('"').repeated().configure(|cfg, ctx| cfg.exactly(*ctx));
-        let content = any().and_is(just('"').not()).then(any().and_is(delim_end.not()).repeated().collect::<String>()).map(|(first, remainder)| format!("{}{}", first, remainder));
-        let longquote_string = delim_start
-            .ignore_with_ctx(content.then_ignore(delim_end))
-            .padded()
-            .map(|s| StringExpr(s));
+        // let delim_start = just('"').repeated().exactly(1).ignore_then(just("\"\"").repeated().count().map(|count| count * 2 + 1)).or(just('"').not().map(|_| 0));
+        // let delim_end = just('"').repeated().configure(|cfg, ctx| cfg.exactly(*ctx));
+        // let content = any().and_is(just('"').not()).then(any().and_is(delim_end.not()).repeated().collect::<String>()).map(|(first, remainder)| format!("{}{}", first, remainder));
+        // let longquote_string = delim_start
+        //     .ignore_with_ctx(content.then_ignore(delim_end))
+        //     .padded()
+        //     .map(|s| StringExpr(s));
 
-        choice(
-            (longquote_string, empty_string)
-        )
+        // choice(
+        //     (longquote_string, empty_string)
+        // )
     });
 }
